73e86f83fcfed54057506104fa4e5413bbd6b640
stackbaxter <stackbaxter@gmail.com>
2017-09-07 02:53:45 -0500

    container comment

--- client/src/components/CommentDetail.js
+++ client/src/components/CommentDetail.js
@@ -4,6 +4,19 @@ export default class CommentDetail extends Component {
   constructor(props) {
     super(props);
   }
+componentDidMount(){
+  let id = this.props.comment.id;
+  this.props.getComment(id);
+}
 
-  
+render(){
+  let comment = this.props.comment.body;
+  return(
+
+    {comment}
+
+  );
+}
+
+
 }
